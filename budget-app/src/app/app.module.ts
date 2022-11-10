2979fb4997bd21076e03b57faa54b254142e76aa
zafiera <zafiera.davids@standardbank.co.za>
2022-11-10 13:41:53 +0200

    update

--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -4,11 +4,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { NavbarComponent } from './navbar/navbar.component';
+import { HomeComponent } from './home/home.component';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC8ADWD5WAiCqUvQUd8W2VVfXcczVUR15A",
+  authDomain: "buddgety.firebaseapp.com",
+  projectId: "buddgety",
+  storageBucket: "buddgety.appspot.com",
+  messagingSenderId: "262349666241",
+  appId: "1:262349666241:web:1509a616597f2e725a2140",
+  measurementId: "G-TLLG5RS3GB"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent
+    LoginComponent,
+    NavbarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
