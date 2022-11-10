eb9aff6ef4b2b63dc077dff020594288cc23d6c3
zafiera <zafiera.davids@standardbank.co.za>
2022-11-10 16:56:46 +0200

    additional components

--- budget-app/src/app/app-routing.module.ts
+++ budget-app/src/app/app-routing.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PricingComponent } from "./pricing/pricing.component";
+import { DashboardSummaryComponent } from "./dashboard-summary/dashboard-summary.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'pricing', component: PricingComponent },
+  { path: 'dashboard-overview', component: DashboardSummaryComponent },
 ];
 
 @NgModule({
--- budget-app/src/app/app.module.ts
+++ budget-app/src/app/app.module.ts
@@ -1,14 +1,21 @@
-import { NgModule } from '@angular/core';
+import {CUSTOM_ELEMENTS_SCHEMA, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
+import { MaterialModule } from './material.module'
+
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+
+import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { HomeComponent } from './home/home.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { PricingComponent } from './pricing/pricing.component';
+import { DashboardSummaryComponent } from './dashboard-summary/dashboard-summary.component';
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -32,13 +39,19 @@ const analytics = getAnalytics(app);
     AppComponent,
     LoginComponent,
     NavbarComponent,
-    HomeComponent
+    HomeComponent,
+    DashboardComponent,
+    PricingComponent,
+    DashboardSummaryComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
